aa827e024a673a0e84ebaa315682986ef9691a11
dtoropin <dennis.tor@gmail.com>
2017-10-19 14:50:35 +0300

    add block partnerFood

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,15 @@
+; // Start custom code
 function setEqualHeight(column) {
-		var mainDivs = $(column); //Получаем все элементы с классом column
-		var maxHeight = 0;
-		for (var i = 0; i < mainDivs.length; ++i) {
-			if (maxHeight < $(mainDivs[i]).height()) { //Находим максимальную высоту
-				maxHeight = $(mainDivs[i]).height();
-			}
+	var mainDivs = $(column);
+	var maxHeight = 0;
+	for (var i = 0; i < mainDivs.length; ++i) {
+		if (maxHeight < $(mainDivs[i]).height()) {
+			maxHeight = $(mainDivs[i]).height();
 		}
-		for (var i = 0; i < mainDivs.length; ++i) {
-			$(mainDivs[i]).height(maxHeight); //Устанавливаем всем элементам максимальную высоту
-		}
+	}
+	for (var i = 0; i < mainDivs.length; ++i) {
+		$(mainDivs[i]).height(maxHeight);
+	}
 }
 
 $(document).ready(function () {
