33526db85fffc60a73f1abbfee2ac2d83f91cc98
dtoropin <dennis.tor@gmail.com>
2017-10-28 00:43:18 +0300

    finished header

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,8 @@
 ; // Start custom code
 $(document).ready(function () {
 	$('.column').matchHeight();
+
+	$('#service').click(function(){
+		$('.header-center').slideToggle();
+	});
 });
--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,8 @@
 ; // Start custom code
 $(document).ready(function () {
 	$('.column').matchHeight();
+
+	$('#service').click(function(){
+		$('.header-center').slideToggle();
+	});
 });
